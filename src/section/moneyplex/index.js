42aa15164da8744c57d24488a66defd944d440d3
githubacct0 <githubacct0@outlook.com>
2022-04-30 03:31:48 +0900

    remove unnecessary package

--- src/section/moneyplex/index.js
+++ src/section/moneyplex/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
